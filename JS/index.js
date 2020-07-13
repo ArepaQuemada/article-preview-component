5ca7ebd60f54e1780d782812ada6373bc828dc95
Juan Rigual <RigualJuan@gmail.com>
2020-07-12 22:11:14 -0300

    first commit

---
+++ JS/index.js
@@ -0,0 +1,4 @@
+const shareBox = document.querySelector(".share-box");
+const button = document.querySelector(".centered-button").addEventListener("click", () => {
+    shareBox.hidden = !shareBox.hidden;
+})
