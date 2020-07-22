b1e34cebdda6fcd8d751518b69f41ef32e886e32
Juan Rigual <RigualJuan@gmail.com>
2020-07-22 19:35:23 -0300

    Added cursor pointer

--- JS/index.js
+++ JS/index.js
@@ -1,4 +1,4 @@
 const shareBox = document.querySelector(".share-box");
 const button = document.querySelector(".centered-button").addEventListener("click", () => {
     shareBox.hidden = !shareBox.hidden;
-})
+});
